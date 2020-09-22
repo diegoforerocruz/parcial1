4d363baa15cafb8a23eb075d379c791b62a8c458
Diego Forero Cruz <df.foreroc@uniandes.edu.co>
2020-09-22 18:59:19 -0500

    Terminado

--- scripts.js
+++ scripts.js
@@ -1,19 +1,14 @@
-
 document.getElementById("BurguersLink").addEventListener("click", function () {renderFood("Burguers");} );
 document.getElementById("Tacos").addEventListener("click", function () {renderFood("Tacos");} );
 document.getElementById("Salads").addEventListener("click", function () {renderFood("Salads");} );
 document.getElementById("Desserts").addEventListener("click", function () {renderFood("Desserts");} );
 document.getElementById("Drinks").addEventListener("click", function () {renderFood("Drinks and Sides");} );
 document.getElementById("img-navbar").addEventListener("click", function () {showOrder();} );
-
 document.getElementById("modalContinueAdding").addEventListener("click", function () {continueAdding();} );
 document.getElementById("modalCancelOrder").addEventListener("click", function () {dropOrderTable();} );
 document.getElementById("confirmOrder").addEventListener("click", function () {confirmOrderResult();} );
-
-
 async function getEvents() {
     let url = 'https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json'
-
     try {
         let res = await fetch(url)
         return await res.json();
@@ -21,7 +16,6 @@ async function getEvents() {
         console.log(error)
     }
 }
-
 async function renderFood(tipoComida) {
     let eventos = await getEvents()
     let comida
@@ -30,20 +24,13 @@ async function renderFood(tipoComida) {
         if(obj == tipoComida) {
             comida = evento.products
         }
-        
     })
-        
-
     let html = ''
     let cont = 1
     let htmlSegment = ''
     let container = document.getElementById("foodCards")
     container.innerHTML = ''
-
     comida.map(plato => {
-        
-
-
         htmlSegment=   `<div class="col-3">
                         <div class="card border-left-primary shadow h-100" id="card${cont}">
                             <img src="${plato.image}"  class="card-img-top rounded imgcard" alt="img${cont}">
@@ -55,43 +42,25 @@ async function renderFood(tipoComida) {
                             </div>
                         </div>
                     </div>`
-                    
-        
         container.innerHTML += htmlSegment
-
-        
-
         cont ++
-        
     })
-
     document.getElementById("nombreComida").innerText = tipoComida
-
     for(let i = 1 ; i<cont ; i++) {
         document.getElementById("cardButton"+i).addEventListener("click", function () {modifyCartNumber(i);} );
     }
-
-
 }
-
 function modifyCartNumber(cont){
-    
-    
     elementos = document.getElementById("card"+cont).childNodes[3].childNodes
-
     let table = document.getElementById("oderDetailTable");
     let rows = table.rows
-
     descT = elementos[1].innerText
     priceT = parseFloat(elementos[5].innerText.split(" ")[1])
     qtyT = 1
     AmountT = qtyT*priceT
-
     let row
-    
     if(rows.length == 1) {
-        row = table.insertRow(rows.length)
-            
+        row = table.insertRow(rows.length)      
         row.insertCell(0).innerHTML = rows.length-1;
         row.insertCell(1).innerHTML = qtyT;
         row.insertCell(2).innerHTML = descT;
@@ -105,8 +74,7 @@ function modifyCartNumber(cont){
             }
         }
         if(ward == -1) {
-            row = table.insertRow(rows.length)
-            
+            row = table.insertRow(rows.length)   
             row.insertCell(0).innerHTML = rows.length-1;
             row.insertCell(1).innerHTML = qtyT;
             row.insertCell(2).innerHTML = descT;
@@ -119,27 +87,21 @@ function modifyCartNumber(cont){
             modifyRow.cells[1].innerHTML = cantidad
         }
     }
-
     cartString = document.getElementById("cartText").innerText
     cartNumber = rows.length-1;
     document.getElementById("cartText").innerHTML ="<img src=\"carrito.png\"  id=\"img-navbar\" alt=\"carrito\">" +cartNumber + " Items"
     document.getElementById("img-navbar").addEventListener("click", function () {showOrder();} );
-
     let totalCash = 0
     for(let k = 1; k<rows.length; k++){
         totalCash += parseFloat(rows[k].cells[4].innerHTML)
     }
     document.getElementById("totalaccount").innerHTML = `<p><strong>Total: $ ${totalCash}</strong></p>`
     document.getElementById("oderDetailTable").className = "table table-striped"
-
 }
-
 function continueAdding() {
     document.getElementById("foodDetail").className = "d-block"
     document.getElementById("oderDetailSection").className ="d-none"
-
 }
-
 function dropOrderTable() {
     table = document.getElementById("oderDetailTable")
     while(table.rows.length!=1){
@@ -149,7 +111,6 @@ function dropOrderTable() {
     document.getElementById("img-navbar").addEventListener("click", function () {showOrder();} );
     document.getElementById("totalaccount").innerHTML = `<p><strong>Total: $ 0</strong></p>`
 }
-
 function confirmOrderResult() {
     table = document.getElementById("oderDetailTable")
     rows = table.rows
@@ -157,20 +118,14 @@ function confirmOrderResult() {
     for(let i = 1; i<rows.length; i++) {
         let subArray = ({
             "item": parseInt(rows[i].cells[0].innerHTML),
-            
             "quantity": parseInt(rows[i].cells[1].innerHTML),
-            
             "description": rows[i].cells[2].innerHTML,
-            
             "unity Price": parseFloat(rows[i].cells[3].innerHTML)
         })  
         array.push(subArray)
-        
     }
     console.log(array)
 }
-
-
 function showOrder() {
     document.getElementById("foodDetail").className = "d-none"
     document.getElementById("oderDetailSection").className ="d-block"
